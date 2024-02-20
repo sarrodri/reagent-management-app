0c913a30db73cd4231bf36df5cae0c2b61ae7626
sarrodri <114839018+sarrodri@users.noreply.github.com>
2024-02-19 18:41:03 -0600

    database example setup

---
+++ app.py
@@ -0,0 +1,3 @@
+from flask import Flask
+
+# This is where we will make the API using Flask and Python
---
+++ db.py
@@ -0,0 +1,24 @@
+# https://docs.sqlalchemy.org/en/20/intro.html SQLAlchemy docs
+# Note: Whoever works on this will need to run "pip install -U Flask-SQLAlchemy" into this directory
+# Use comments please and thank youuuu
+
+from flask_sqlalchemy import SQLAlchemy as sql
+import app
+
+#import
+db = sql(app)
+
+#Create a reagent class 
+class Reagent(db.Model):
+    # in here, we will shape the data from the api into a database
+    #Example (I think):
+    
+    emplid = db.Column(db.String(100), nullable=False)
+    lot_number = db.Column(db.String(50))
+    expiration_date = db.Column(db.Date)
+    storage_location = db.Column(db.String(100))
+    
+#Metadata and view table object
+metadata = db.MetaData() #extracting the metadata
+division= db.Table('divisions', metadata, autoload=True, autoload_with = "engine")
+print(repr(metadata.tables['divisions']))

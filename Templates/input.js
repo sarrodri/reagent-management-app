bca89f233e8a3692e09832c3cc883431b550f711
GravitationalNull <thespaman723@gmail.com>
2024-04-05 10:00:22 -0500

    attempting functions, api and stuff

--- Templates/input.js
+++ Templates/input.js
@@ -1,6 +1,29 @@
-//
-//import 
-barcodeIn() {
+//look at getElementsByClassName function for showing the data in the window (index from like 0 to 29)
+//import
+function TempSubmitForm(){
+    let ClockFaceTime = document.getElementById('ClockFaceTime').value
+    let reagent_names = document.getElementById('reagent_names').value
+    console.log(ClockFaceTime)
+    console.log(reagent_names)
+}
+
+function barcodeIn() {
     let newBarcode = prompt("Scan Barcode");
     print(newBarcode);
+}
+
+function SubmitForm(){
+
+}
+
+function Undo(){
+
+}
+
+function UndoValidation(){
+
+}
+
+function getReagentNames(reagent_names){
+    reagent_names
 }
